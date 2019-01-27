2e3b641039819629ffa37ab09ad47d9c9f5b3bc9
SoleSun <joel.ahn@alumni.ubc.ca>
2019-01-26 19:14:30 -0800

    Created Grass Detection Module

---
+++ Grass_Detection/Detect_Grass.java
@@ -0,0 +1,12 @@
+/*
+ * Empower Green color regions by for example using the Exessive Green formula.
+ * Then segment green regions and create a binary mask.
+ * If the mask has blobs that are larger then x pixels in the movement direction, 
+ * then classify as object, else it is classified as grass
+ */
+public class Detect_Grass{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello, World!");
+	}
+}
