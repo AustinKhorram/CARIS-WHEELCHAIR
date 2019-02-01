31dd3a9275c93bbd5bc89a874564043241ecab83
KeenanMcConkey <k.t.mcconkey@gmail.com>
2019-01-31 17:25:18 -0800

    Audio fixes

--- audio/run_wav.py
+++ audio/run_wav.py
@@ -1,27 +1,46 @@
 # Implemented functions
-from wav_functions import get_freq
+from wav_functions import get_array
 from wav_functions import write_freq
+from wav_functions import normalized_fft
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Test reading some files
 gravel_file = "gravel.wav"
 pavement_file = "pavement.wav"
 test_file = "test.wav"
-pavement_data = get_freq(pavement_file)
-gravel_data = get_freq(gravel_file)
+
+pavement_data = get_array(pavement_file)
+gravel_data = get_array(gravel_file)
+pavement_fft = normalized_fft(pavement_data)
+gravel_fft = normalized_fft(gravel_data)
+pavement_mean = 100*np.abs(np.average(pavement_data))
+gravel_mean = np.abs(np.average(gravel_data))
 
 # Plot two fft'd wav file
-fig, ax = plt.subplots(nrows=1, ncols=2)
+fig_f, ax = plt.subplots(nrows=2, ncols=2)
 
-ax[0].plot(gravel_data)
-ax[0].set_xlabel("f")
-ax[0].set_ylabel("A")
-ax[0].set_title("FFT of .wav file \"%s\"" % gravel_file)
+ax[0][0].plot(gravel_data, label="Mean A = %s" % gravel_mean)
+ax[0][0].legend()
+ax[0][0].set_xlabel("t")
+ax[0][0].set_ylabel("A")
+ax[0][0].set_title("Audio \"%s\"" % gravel_file)
 
-ax[1].plot(pavement_data)
-ax[1].set_xlabel("f")
-ax[1].set_ylabel("A")
-ax[01].set_title("FFT of .wav file \"%s\"" % pavement_file)
+ax[0][1].plot(pavement_data, label="Mean A = %s" % pavement_mean)
+ax[0][1].legend()
+ax[0][1].set_xlabel("t")
+ax[0][1].set_ylabel("A")
+ax[0][1].set_title("Audio \"%s\"" % pavement_file)
+
+ax[1][0].plot(gravel_fft)
+ax[1][0].set_xlabel("f")
+ax[1][0].set_ylabel("A")
+ax[1][0].set_title("Normalized FFT of \"%s\"" % gravel_file)
+
+ax[1][1].plot(pavement_fft)
+ax[1][1].set_xlabel("f")
+ax[1][1].set_ylabel("A")
+ax[1][1].set_title("Normalized FFT of \"%s\"" % pavement_file)
 
 plt.show()
 
