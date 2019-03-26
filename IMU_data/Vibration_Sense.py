e25eb64eed8d4d9b368d0b77a5ee1e9d9b4ad0d0
SoleSun <joel.ahn@alumni.ubc.ca>
2019-03-25 23:24:34 -0700

    Created visualization functionality for peak variances. Attempting to determine centroids.

--- IMU_data/Vibration_Sense.py
+++ IMU_data/Vibration_Sense.py
@@ -8,6 +8,7 @@
 
 from scipy.signal import find_peaks
 import numpy as np
+import tensorflow as tf 
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
@@ -19,22 +20,26 @@ import os
 # The current source directory is inside the folder housing this script
 SOURCE_DIR = "."
 
-TIMESTAMP_INDEX = 0
-X_ACC_INDEX     = 1
-Y_ACC_INDEX     = 2
-Z_ACC_INDEX     = 3
+X_ACC_INDEX     = 0
+Y_ACC_INDEX     = 1
+Z_ACC_INDEX     = 2
+TIMESTAMP_INDEX = 9
+TITLES          = ["X Acceleration", "Y Acceleration", "Z Acceleration", \
+                   "X Linear Acceleration", "Y Linear Acceleration", "Z Linear Acceleration", \
+                   "X Gyroscope", "Y Gyroscope", "Z Gyroscope"]
 
 # Path to directory detailing the location of the IMU data 
 # The data is organized according to a comma separate values (csv)
 # The first column represents the time stamp, the subsequent rows represent
 # the linear accelerations sampled in the x, y, and z axes
-def parseCSV(pathToFile, noOfColumns):
+def parseCSVAndRotate(pathToFile, noOfColumns, delimiter, readFromLine):
 	
 	# Final return value is a rotated matrix of the CSVC
 	# i.e. the columns are mapped as rows and columns as rows
 	#   
 	# time:  [[t1 t2  ... tn],
 	# x acc:  [x1 x2  ... xn],
+
 	# y acc:  [y1 y2  ... yn],
 	# z acc:  [z1 z2  ... zn]]
 
@@ -44,15 +49,23 @@ def parseCSV(pathToFile, noOfColumns):
 		data.append([])
 
 	textfile = open(pathToFile)
-	for line in textfile:
+	for i, line in enumerate(textfile):
+		if (i < readFromLine):
+			continue 
 		# Separate the line around the comma ',' and then append the 
 		# row data to the overall data set
-		row_data = line.strip("\n").split(",")
+		row_data = line.strip("\n").split(delimiter)
 		# Ignore the last element in the list because it is empty
 		for i, dataPoint in enumerate(row_data[:-1]):
 			data[i].append(dataPoint)
 
 	return data
+
+def splitArrayIntoNEvenSubArrays(data_set, no_of_groups=1):
+    length = len(data_set)
+    return [ data_set[i*length // no_of_groups: (i+1)*length // no_of_groups] 
+             for i in range(no_of_groups) ]
+
 
 def findVarianceOfPositivePeaks(data_set):
 	# Tuning parameters of the IMU analysis: 
@@ -75,8 +88,23 @@ def findVarianceOfNegativePeaks(data_set):
 	flipped_data_set = [str(float(x) * -1) for x in data_set]
 	return findVarianceOfPositivePeaks(flipped_data_set) 
 
-def plotAcc (time, accX, accY, accZ, title):
-	print(title)
+### FFT-Algorithm Relevant Functions ###
+
+def dataSetIsSizePowerOf2(data_set):
+	size = len(data_set)
+	return size > 0 and ((size & (size - 1)) == 0)
+
+def padZeros(data_set, size):
+	"""
+	If the data_set has a size that's not a power of 2 then 
+	pad it with an equal number of zeros in the front and back 
+	"""
+	zeros_left   = tf.zeros([int(data_set.get_shape()[0]), int((N - input_length+1) / 2)])
+	zeros_right  = tf.zeros([int(data_set.get_shape()[0]), int((N - input_length) / 2)])
+	input_padded = tf.concat([zeros_left, input, zeros_right], axis=1)
+
+### Data Visualization Helper Functions ### 
+def plotAcc (time, accX, accY, accZ):
 	plt.subplot(2,2,1),plt.plot(time, accX, 'r')
 	plt.title('X Acceleration') #plt.xticks([]), plt.yticks([])
 	plt.subplot(2,2,2),plt.plot(time, accY, 'g')
@@ -85,20 +113,52 @@ def plotAcc (time, accX, accY, accZ, title):
 	plt.title('Z Acceleration') 
 	plt.show()
 
-for root, dirnames, filenames in os.walk(SOURCE_DIR):
-	for filename in filenames:
-		if filename.endswith(('.txt')):
-			IMU_data = parseCSV(filename, 4)
+### MAIN ###
+def analyze():
+	for root, dirnames, filenames in os.walk(SOURCE_DIR):
+		for filename in filenames:
+			if filename.endswith(('.txt','.csv')):
+				print(filename)
 
-			"""
-			# Plot all three acceleration graphs 
-			plotAcc(IMU_data[TIMESTAMP_INDEX], \
-					IMU_data[X_ACC_INDEX],     \
-				    IMU_data[Y_ACC_INDEX],     \
-				    IMU_data[Z_ACC_INDEX],
-				    filename)
-			"""
-			print(IMU_data[X_ACC_INDEX])
-			print([str(float(x) * -1) for x in IMU_data[X_ACC_INDEX]])
-			
-			plt.show(block=True)
+				IMU_data = parseCSVAndRotate(filename,       \
+					                         noOfColumns=10, \
+					                         delimiter=";",  \
+					                         readFromLine=2)
+
+				for i in range(1,3):
+					# Plot all three acceleration graphs 
+					plotAcc(IMU_data[TIMESTAMP_INDEX],   \
+							IMU_data[X_ACC_INDEX + i*3], \
+						    IMU_data[Y_ACC_INDEX + i*3], \
+						    IMU_data[Z_ACC_INDEX + i*3])
+
+				# Now find the positive and negative variances of the peaks
+				# for each discrete IMU data set and plot the two dimensional 
+				# graph, noting any discrepancies
+				# The last index carries the time stamp so don't find the variance
+				# of that one  
+				PEAK_VAR_SAMPLE_SIZE = 50
+
+				for i, data_set in enumerate([IMU_data[5],IMU_data[8]]):
+					# Take each variance to be a sample size of 50 and then plot
+					# it accordingly 
+					sub_data_sets = splitArrayIntoNEvenSubArrays(data_set, \
+						                                        len(data_set) // PEAK_VAR_SAMPLE_SIZE)
+					pos_variances = []
+					neg_variances = []
+
+					for sub_data_set in sub_data_sets:
+						pos_variances.append(findVarianceOfPositivePeaks(sub_data_set))
+						neg_variances.append(findVarianceOfNegativePeaks(sub_data_set))
+
+					plt.plot(neg_variances, pos_variances, 'ro')
+					plt.title(TITLES[i])
+					plt.show()
+
+					#Try to also run FFT on the data set
+					
+
+
+
+if __name__ == '__main__':
+	analyze()	
